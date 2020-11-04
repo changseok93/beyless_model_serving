d5557c8a3cd8bf1f69f26dfe1125c3fc00c0dce2
changseok <changseok93@icloud.com>
2020-11-04 08:45:49 +0000

    add docker

---
+++ docker/bootstrap.sh
@@ -0,0 +1,11 @@
+docker build --build-arg USER_ID=$UID -t detectron2_beyless_header:v0.1 .
+
+docker run --gpus all -it \
+	-e LC_ALL=C.UTF-8 \
+	-p 50051:50051 \
+	-p 6006:6006 \
+	-p 8888:8888 \
+	--shm-size=8gb --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	--name=detectron2_beyless_header detectron2_beyless_header:v0.1
+
+
